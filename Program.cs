16df803d3811904643e4693cb96ea13dd305af55
Kyle Jensen <kyle.w.jensen1@gmail.com>
2017-05-25 21:59:29 -0500

    Added a Player class, a static method on GameResult class and Player class for deserialization

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace SoccerStats {
     class Program {
@@ -12,6 +13,12 @@ namespace SoccerStats {
             DirectoryInfo directory = new DirectoryInfo(currDirectory);
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
             var fileContents = ReadSoccerResults(fileName);
+            fileName = Path.Combine(directory.FullName, "players.json");
+            var players = DeserializePlayers(fileName);
+
+            foreach(var player in players) {
+                Console.WriteLine(player.FirstName);
+            }
         }
         public static string ReadFile(string fileName) {
             using(var reader = new StreamReader(fileName)) {
@@ -19,16 +26,54 @@ namespace SoccerStats {
             }
         }
 
-        public static List<string[]> ReadSoccerResults(string fileName) {
-            var soccerResults = new List<string[]>();
+        public static List<GameResult> ReadSoccerResults(string fileName) {
+            var soccerResults = new List<GameResult>();
             using (var reader = new StreamReader(fileName)) {
                 string line = "";
+                reader.ReadLine();
                 while ((line = reader.ReadLine()) != null) {
+                    var gameResult = new GameResult();
                     string[] values = line.Split(',');
-                    soccerResults.Add(values);
+                    DateTime gameDate;
+                    if (DateTime.TryParse(values[0], out gameDate)) {
+                        gameResult.GameDate = gameDate;
+                    }
+                    gameResult.TeamName = values[1];
+                    HomeOrAway homeOrAway;
+                    if(Enum.TryParse(values[2], out homeOrAway)) {
+                        gameResult.HomeOrAway = homeOrAway;
+                    }
+                    int parseInt;
+                    if(int.TryParse(values[3], out parseInt)) {
+                        gameResult.Goals = parseInt;
+                    }
+                    if (int.TryParse(values[4], out parseInt)) {
+                        gameResult.GoalAttempts = parseInt;
+                    }
+                    if (int.TryParse(values[5], out parseInt)) {
+                        gameResult.ShotsOnGoal = parseInt;
+                    }
+                    if (int.TryParse(values[6], out parseInt)) {
+                        gameResult.ShotsOffGoal = parseInt;
+                    }
+                    double posessionsPercent;
+                    if (double.TryParse(values[7], out posessionsPercent)) {
+                        gameResult.PosessionPercent = posessionsPercent;
+                    }
+                    soccerResults.Add(gameResult);
                 }
             }
             return soccerResults;
+        }
+
+        public static List<Player> DeserializePlayers(string fileName) {
+            var players = new List<Player>();
+            var serializer = new JsonSerializer();
+            using (var reader = new StreamReader(fileName))
+            using (var jsonReader = new JsonTextReader(reader)) {
+               players = serializer.Deserialize<List<Player>>(jsonReader);
+            }
+            return players;
         }
     }
 }
